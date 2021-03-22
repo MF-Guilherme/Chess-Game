674e4b2f248c30cfe88347d47c313fd272c70907
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2021-03-22 14:03:18 -0300

    Class Position created

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using Board;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine( "Hello World!" );
+
+            Position P;
+
+            P = new Position( 3 , 4 );
+
+            Console.WriteLine("Position: " + P);
+
+            Console.ReadLine();
+
         }
     }
 }
