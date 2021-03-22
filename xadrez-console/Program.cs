9482e07b52786477e979c86b29c3da8086ab9d89
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2021-03-22 14:40:37 -0300

    Starting to implement the board

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using Board;
+using board;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            Position P;
-
-            P = new Position( 3 , 4 );
-
-            Console.WriteLine("Position: " + P);
+            Board board = new Board(8 , 8);
 
             Console.ReadLine();
 
